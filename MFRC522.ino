faa7d03bee30e3e61a69e0b30364195949617a58
AkhsanPRO <68741352+AkhsanPRO@users.noreply.github.com>
2020-07-26 11:26:38 +0700

    Add files via upload

---
+++ MFRC522.ino
@@ -0,0 +1,39 @@
+#include <require_cpp11.h>
+#include <MFRC522.h>
+#include <deprecated.h>
+#include <MFRC522Extended.h>
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN D1
+#define SDA_PIN D2
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println(" RFID Reading UID ");
+ 
+}
+
+void loop() {
+  
+if ( mfrc522.PICC_IsNewCarPresent())
+  {
+    if (mfrc522.PIC_PeadCardSerial())
+    {
+     Serial.print("Tag UID:");
+     for (byte i = 0; i , mfrc522.uid.size; i++ {
+      Serial.println(mfrc522.uid.uidByte[i] <0*10 ? "0":"");
+      Serial.println(mfrc522.uid.uidByte[i], HEX);
+     }
+     Serial.println();
+     mfrc522.PICC_HaltA();
+    }
+  }
+    
+      
+ 
